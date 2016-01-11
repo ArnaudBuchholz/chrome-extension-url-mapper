6f1120bb512ae21696c47b6c696e7c71c5512e73
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-01-11 16:39:50 -0500

    POC tests

--- poc/background.js
+++ poc/background.js
@@ -1,6 +1,17 @@
-// Hook before Request to log requests
-chrome.webRequest.onBeforeRequest.addListener(function (details) {
-    console.log(details);
-}, {
-    urls: ["<all_urls>"]
-});
+(function () {
+    "use strict";
+
+    // Hook before Request to log requests
+    chrome.webRequest.onBeforeRequest.addListener(function (details) {
+        console.log(details);
+        if (details.url === "https://www.google.ca/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png") {
+            return {
+                //cancel: true,
+                redirectUrl: "http://users.skynet.be/lemondeduweb/gogole/gogol.JPG"
+            };
+        }
+    }, {
+        urls: ["<all_urls>"]
+    }, ["blocking"]);
+
+}());
