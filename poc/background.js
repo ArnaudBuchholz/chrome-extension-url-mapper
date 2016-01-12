84920259e15f7efa91eca4c4f38321e81c5ed213
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-01-12 16:21:27 -0500

    Basic communication update

--- poc/background.js
+++ poc/background.js
@@ -1,6 +1,21 @@
 (function () {
     "use strict";
 
+    var configPerTabId = {};
+
+    // Message handling
+    chrome.extension.onConnect.addListener(function (port) {
+        port.onMessage.addListener(function (msg) {
+            if ("query-status" === msg.type) {
+                var config = configPerTabId[msg.tabId];
+                port.postMessage({
+                    type: "status",
+                    enabled: undefined !== config && config.enabled
+                });
+            }
+        });
+    });
+
     // Hook before Request to log requests
     chrome.webRequest.onBeforeRequest.addListener(function (details) {
         console.log(details);
--- poc/popup.js
+++ poc/popup.js
@@ -1,10 +1,36 @@
 (function () {
     "use strict";
 
-    /*
-http://stackoverflow.com/questions/13546778/how-to-communicate-between-popup-js-and-background-js-in-chrome-extension
-     */
-    document.addEventListener("DOMContentLoaded", function () {
+    // Message handling
+    var backgroundPort = chrome.extension.connect({name: "chrome-extension-url-mapper"});
+    backgroundPort.onMessage.addListener(function (msg) {
+        if ("status" === msg.type) {
+            var newClassName = "switch ";
+            if (msg.enabled) {
+                newClassName += "on";
+                chrome.browserAction.setBadgeText({text: "ON"});
+            } else {
+                newClassName += "off";
+                chrome.browserAction.setBadgeText({text: ""});
+            }
+            document.querySelector(".switch").className = newClassName;
+        }
+    });
+
+    function _onInit () {
+
+        var tabId;
+
+        // Grab current tab
+        chrome.tabs.getSelected(null, function (tab) {
+            tabId = tab.id;
+            document.querySelector(".tabId").innerHTML = tabId;
+            // Query status
+            backgroundPort.postMessage({
+                type: "query-status",
+                tabId: tabId
+            });
+        });
 
         document.getElementById("source").onchange = function () {
             document.querySelector("span.name").innerHTML = this.value;
@@ -14,12 +40,19 @@ http://stackoverflow.com/questions/13546778/how-to-communicate-between-popup-js-
             window.close();
         };
 
-        document.getElementById("test").addEventListener("click", function () {
-            chrome.runtime.sendMessage({test: "hello world!"}, function(response) {
-                alert(response);
-                window.close();
-            });
+        document.querySelector("span.name").addEventListener("click", function () {
+            alert("YO!");
         });
-    });
+
+        document.querySelector(".switch").addEventListener("click", function () {
+            alert("YO!");
+        });
+
+    }
+
+    /*
+http://stackoverflow.com/questions/13546778/how-to-communicate-between-popup-js-and-background-js-in-chrome-extension
+     */
+    document.addEventListener("DOMContentLoaded", _onInit);
 
 }());
