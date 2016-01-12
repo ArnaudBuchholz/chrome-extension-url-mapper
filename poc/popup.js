ee965c7d4eadd45cb47205693ffeb84215ba0644
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-01-12 15:43:40 -0500

    WIP for configuration dialog

--- poc/popup.js
+++ poc/popup.js
@@ -1,9 +1,19 @@
 (function () {
     "use strict";
 
-    // http://stackoverflow.com/questions/13546778/how-to-communicate-between-popup-js-and-background-js-in-chrome-extension
+    /*
+http://stackoverflow.com/questions/13546778/how-to-communicate-between-popup-js-and-background-js-in-chrome-extension
+     */
+    document.addEventListener("DOMContentLoaded", function () {
 
-    document.addEventListener('DOMContentLoaded', function() {
+        document.getElementById("source").onchange = function () {
+            document.querySelector("span.name").innerHTML = this.value;
+            chrome.browserAction.setTitle({title: this.value});
+            chrome.browserAction.setBadgeText({text: "ON"});
+            chrome.browserAction.setBadgeBackgroundColor({color: "#FF0000"});
+            window.close();
+        };
+
         document.getElementById("test").addEventListener("click", function () {
             chrome.runtime.sendMessage({test: "hello world!"}, function(response) {
                 alert(response);
