df3a0ba63c3151721f7150d12fd4d890910fc137
Arnaud Buchholz <arnaud.buchholz@free.fr>
2016-01-17 23:56:06 -0500

    Set configuration message

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,9 @@
 (function () {
     "use strict";
 
-    um.EXTENSION_NAME       = "chrome-extension-url-mapper";
+    um.EXTENSION_NAME           = "chrome-extension-url-mapper";
 
-    um.MSG_QUERY_STATUS     = "query-status";
+    um.MSG_QUERY_STATUS         = "query-status";
+    um.MSG_SET_CONFIGURATION    = "set-configuration";
 
 }());
