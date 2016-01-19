d5b9d1b030e150997d880cac8cd20b28aadeb6e2
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-01-19 14:28:07 -0500

    Mappings loading & matching

--- src/configuration.js
+++ src/configuration.js
@@ -11,6 +11,10 @@
     function Configuration (tabId, configuration) {
         this._tabId = tabId;
         this._name = configuration.name;
+        var mappings = this._mappings = [];
+        configuration.mappings.forEach(function (mapping) {
+            mappings.push(new um.Mapping(mapping));
+        });
     }
 
     Configuration.prototype = {
@@ -20,6 +24,9 @@
 
         // @property {String} name
         _name: "",
+
+        // @property {um.Mapping[]} array of mappings
+        _mappings: [],
 
         // @property {Boolean} True if the configuration is enabled
         _isEnabled: false,
@@ -42,6 +49,35 @@
 
         disable: function () {
             this._isEnabled = false;
+        },
+
+        processRequest: function (request) {
+            if (!this._isEnabled) {
+                return; // undefined => nothing
+            }
+            var idx,
+                len = this._mappings.length,
+                mapping,
+                matchResult;
+            for (idx = 0; idx < len; ++idx) {
+                mapping = this._mappings[idx];
+                matchResult = mapping.match(request);
+                if (matchResult) {
+                    return mapping.process(request, matchResult);
+                }
+            }
+
+
+
+/*
+            console.log(details);
+            if (0 === details.url.indexOf("https://www.google.ca/images/branding/googlelogo/")) {
+                return {
+                    //cancel: true,
+                    redirectUrl: "http://users.skynet.be/lemondeduweb/gogole/gogol.JPG"
+                };
+            }
+*/
         }
 
     };
