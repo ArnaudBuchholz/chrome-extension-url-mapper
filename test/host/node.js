6a6aa8ae4fe8e478b17979531207ecf8df17f81a
Arnaud Buchholz <arnaud.buchholz@free.fr>
2016-01-20 00:07:09 -0500

    Node environment

---
+++ test/host/node.js
@@ -0,0 +1,23 @@
+(function () {
+    "use strict";
+    /*eslint-env node*/
+
+    // Defines namespace
+    global.um = {};
+
+    // Mocks chrome API
+    global.chrome = require("./chrome.js");
+
+    // Loads components
+    require("./../../src/constants.js");
+    require("./../../src/mapping.js");
+    require("./../../src/configuration.js");
+    require("./../../src/configmap.js");
+    require("./../../src/background.js");
+
+    require("./../../src/popup/PopupController.js");
+
+    // Mocks PopupView
+    um.PopupView = require("./PopupView.js");
+
+}());
