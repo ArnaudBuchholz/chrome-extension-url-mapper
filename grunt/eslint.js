bebe6bb792f2c52b3c9fe3cf05c674e2cd9ecbbe
Arnaud Buchholz <arnaud.buchholz@free.fr>
2016-01-20 00:05:54 -0500

    Grunt eslint

---
+++ grunt/eslint.js
@@ -0,0 +1,9 @@
+"use strict"; //eslint-disable-line strict
+/*eslint-env node*/
+
+module.exports = {
+    options: {
+        configFile: ".eslintrc"
+    },
+    target: ["src/**/*.js", "test/**/*.js"]
+};
