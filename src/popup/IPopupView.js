d51495bc72f7efc1b26db72a56b34fcccc1def29
Arnaud Buchholz <arnaud.buchholz@free.fr>
2016-01-20 00:06:29 -0500

    ESLint fixes

--- src/popup/IPopupView.js
+++ src/popup/IPopupView.js
@@ -1,12 +1,14 @@
 (function () {
     "use strict";
 
+    function _ignore () {}
+
     /**
      * Popup View abstraction
      *
      * @interface
      */
-    function IPopupView() {}
+    function IPopupView () {}
 
     IPopupView.prototype = {
 
@@ -15,35 +17,45 @@
          *
          * @param {String} busy
          */
-        setBusy: function (busy) {},
+        setBusy: function (busy) {
+            _ignore(busy);
+        },
 
         /**
          * Display the current tab identifier
          *
          * @param {String} tabId
          */
-        setTabId: function (tabId) {},
+        setTabId: function (tabId) {
+            _ignore(tabId);
+        },
 
         /**
          * Change ON/OFF Switch
          *
          * @param {Boolean} state True for ON, False for OFF
          */
-        setSwitchState: function (state) {},
+        setSwitchState: function (state) {
+            _ignore(state);
+        },
 
         /**
          * Display an error
          *
          * @param {String} message
          */
-        showError: function (message) {},
+        showError: function (message) {
+            _ignore(message);
+        },
 
         /**
          * Display the current configuration name
          *
          * @param {String} name
          */
-        setConfigurationName: function (name) {}
+        setConfigurationName: function (name) {
+            _ignore(name);
+        }
 
     };
 
