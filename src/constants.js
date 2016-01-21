7d8843ee97fa2aa38bd4d8135d3f8228ecdd166e
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-01-21 15:57:15 -0500

    Default no configuration message

--- src/constants.js
+++ src/constants.js
@@ -3,6 +3,8 @@
 
     um.EXTENSION_NAME                   = "chrome-extension-url-mapper";
 
+    um.MSG_NO_CONFIGURATION             = "(click to select a configuration)";
+
     um.MSG_QUERY_STATUS                 = "query-status";
     um.MSG_SET_CONFIGURATION            = "set-configuration";
     um.MSG_ENABLE_CONFIGURATION         = "enable-configuration";
