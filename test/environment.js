54edd418a120c7cf6e144b3f45f2cc7733b027be
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-01-21 15:58:48 -0500

    Test case to validate the environment

--- test/environment.js
+++ test/environment.js
@@ -3,20 +3,85 @@
 
 /*eslint-disable max-nested-callbacks*/
 
-describe("environment", function () {
+describe("Testing the environment", function () {
     "use strict";
 
-    var view,
-        controller;
+    var URL = "https://github.com/ArnaudBuchholz/chrome-extension-url-mapper";
 
-    beforeEach(function () {
-        view = new um.PopupView();
-        controller = new um.PopupController(view);
-    });
+    describe("Create the tab and simulate the PopupView", function () {
 
-    it("receives and displays a tabId", function () {
-        assert(controller.tabId !== "");
-        assert(view.tabId === controller.tabId);
+        var tab,
+            view,
+            controller;
+
+        before(function (done) {
+            chrome.tabs.create({}, function (newTab) {
+                tab = newTab;
+                view = new um.PopupView();
+                view.whenNoMoreBusy = done;
+                controller = new um.PopupController(view);
+            });
+        });
+
+        it("receives and displays a tabId", function () {
+            assert(tab.id === view.tabId);
+        });
+
+        it("gots an initial state", function () {
+            assert(um.MSG_NO_CONFIGURATION === view.name);
+            assert(false === view.state);
+        });
+
+        describe("Configure the service", function () {
+            before(function (done) {
+                view.whenNoMoreBusy = function () {
+                    view.whenNoMoreBusy = done;
+                    controller.switchState();
+                }
+                controller.setConfiguration(JSON.stringify({
+                    name: "test",
+                    mappings:[{
+                        url: URL,
+                        block: true
+                    }]
+                }));
+            });
+
+            it("shows the configuration name", function () {
+                assert("test" === view.name);
+            });
+
+            it("is enabled", function () {
+                assert(true === view.state);
+            });
+
+            it("processes the web requests", function () {
+                var answer = chrome.webRequest.onBeforeRequest({
+                    tabId: tab.id,
+                    url: URL
+                });
+                assert(true === answer.cancel);
+            });
+
+            describe("Close the tab", function () {
+
+                before(function (done) {
+                    chrome.tabs.remove(tab.id, done);
+                });
+
+                it("cleaned the mapping", function () {
+                    // Border-line: we are not supposed to call this method once the tab is closed
+                    var answer = chrome.webRequest.onBeforeRequest({
+                        tabId: tab.id,
+                        url: URL
+                    });
+                    assert(undefined === answer);
+                });
+
+            });
+
+        });
+
     });
 
 });
