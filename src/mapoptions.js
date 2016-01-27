7fab2eef6cdbd059d41ed5076b58332202e8919a
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-01-27 16:01:24 -0500

    Added debug

--- src/mapoptions.js
+++ src/mapoptions.js
@@ -12,8 +12,20 @@
 
     MappingOptions.prototype = {
 
+        // @property {Boolean} Logs information about the request
+        debug: false,
+
         // @property {Boolean} Change the answer header to inject Access-Control-Allow-Origin: <current website>
         overrideCORS: false,
+
+        /**
+         * Returns true if the options are not the default one
+         *
+         * @returns {Boolean}
+         */
+        areModified: function () {
+            return 0 !== Object.keys(this).length;
+        },
 
         /**
          * Apply the options to the given object
