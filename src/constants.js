017754fda58fe320da90ed6f6d0dfb817241b22c
Arnaud Buchholz <arnaud.buchholz@free.fr>
2016-01-26 19:17:48 -0500

    Declared JSON properties as constants

--- src/constants.js
+++ src/constants.js
@@ -5,6 +5,12 @@
 
     um.MSG_NO_CONFIGURATION             = "(click to select a configuration)";
 
+    um.MAPPING_SETTING_MATCH_URL        = "url";
+    um.MAPPING_SETTING_MATCH_REGEXP     = "match";
+    um.MAPPING_SETTING_REDIRECT_BLOCK   = "block";
+    um.MAPPING_SETTING_REDIRECT_URL     = "redirect";
+    um.MAPPING_SETTING_OPTION_OVR_CORS  = "override-CORS";
+
     um.MSG_QUERY_STATUS                 = "query-status";
     um.MSG_SET_CONFIGURATION            = "set-configuration";
     um.MSG_ENABLE_CONFIGURATION         = "enable-configuration";
