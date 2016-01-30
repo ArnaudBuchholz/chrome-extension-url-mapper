ad2bd839c09f25400fe82d439bdb7a2cfe8c4424
Arnaud Buchholz <arnaud.buchholz@free.fr>
2016-01-30 10:45:02 -0500

    Hook not working

--- src/content/xhr.js
+++ src/content/xhr.js
@@ -1,8 +1,22 @@
 (function () {
     "use strict";
 
-    console.log("chrome-extension-url-mapper/xhr");
+    function _log () {
+        var args = [].slice.call(arguments, 0);
+        args.unshift("chrome-extension-url-mapper");
+        console.log.apply(console, args);
+    }
 
-    console.log(window.XMLHttpRequest.prototype.send);
+    // Override the XMLHttpRequest object
+    var _open = window.XMLHttpRequest.prototype.open;
+    window.XMLHttpRequest.prototype.open = function (method, url, async) {
+        var args = [].slice.call(arguments, 0);
+        if (!async) {
+            _log.apply(null, ["XHR::open not supported"].concat(args));
+        }
+        return _open.apply(this, args);
+    };
+
+    _log("XHR installed");
 
 }());
