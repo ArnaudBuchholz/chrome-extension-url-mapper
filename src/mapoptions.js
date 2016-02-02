a96e4731e32ba55d8705f3176c8995defd7ebee1
Arnaud Buchholz <arnaud.buchholz@free.fr>
2016-02-01 23:31:22 -0500

    overrideCORS is useless

--- src/mapoptions.js
+++ src/mapoptions.js
@@ -15,9 +15,6 @@
         // @property {Boolean} Logs information about the request
         debug: false,
 
-        // @property {Boolean} Change the answer header to inject Access-Control-Allow-Origin: <current website>
-        overrideCORS: false,
-
         /**
          * Returns true if the options are not the default one
          *
