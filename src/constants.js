873943c61a4a7168a122789462a333f53851da20
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-02-04 12:53:43 -0500

    New option for Access-Control-Allow-Headers

--- src/constants.js
+++ src/constants.js
@@ -1,21 +1,21 @@
 (function () {
     "use strict";
 
-    um.EXTENSION_NAME                   = "chrome-extension-url-mapper";
+    um.EXTENSION_NAME                       = "chrome-extension-url-mapper";
 
-    um.MSG_NO_CONFIGURATION             = "(click to select a configuration)";
+    um.MSG_NO_CONFIGURATION                 = "(click to select a configuration)";
 
-    um.MAPPING_SETTING_MATCH_URL        = "url";
-    um.MAPPING_SETTING_MATCH_REGEXP     = "match";
-    um.MAPPING_SETTING_REDIRECT_BLOCK   = "block";
-    um.MAPPING_SETTING_REDIRECT_URL     = "redirect";
-    um.MAPPING_SETTING_OPTION_OVR_CORS  = "override-CORS";
-    um.MAPPING_SETTING_OPTION_DEBUG     = "debug";
+    um.MAPPING_SETTING_MATCH_URL            = "url";
+    um.MAPPING_SETTING_MATCH_REGEXP         = "match";
+    um.MAPPING_SETTING_REDIRECT_BLOCK       = "block";
+    um.MAPPING_SETTING_REDIRECT_URL         = "redirect";
+    um.MAPPING_SETTING_OPTION_DEBUG         = "debug";
+    um.MAPPING_SETTING_OPTION_AC_HEADERS    = "ac-headers"; // Access-Control-Allow-Headers
 
-    um.MSG_INIT_XHR_CONTENT_SCRIPT      = "init-xhr-content-script";
-    um.MSG_QUERY_STATUS                 = "query-status";
-    um.MSG_SET_CONFIGURATION            = "set-configuration";
-    um.MSG_ENABLE_CONFIGURATION         = "enable-configuration";
-    um.MSG_DISABLE_CONFIGURATION        = "disable-configuration";
+    um.MSG_INIT_XHR_CONTENT_SCRIPT          = "init-xhr-content-script";
+    um.MSG_QUERY_STATUS                     = "query-status";
+    um.MSG_SET_CONFIGURATION                = "set-configuration";
+    um.MSG_ENABLE_CONFIGURATION             = "enable-configuration";
+    um.MSG_DISABLE_CONFIGURATION            = "disable-configuration";
 
 }());
