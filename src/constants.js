36c6a327eaeb810282a2f253d877d4b41f6f5938
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-02-08 15:39:53 -0500

    Added missing constants

--- src/constants.js
+++ src/constants.js
@@ -5,6 +5,10 @@
 
     um.MSG_NO_CONFIGURATION                 = "(click to select a configuration)";
 
+    um.CONFIG_NAME                          = "name";
+    um.CONFIG_INJECT                        = "inject";
+    um.CONFIG_MAPPINGS                      = "mappings";
+
     um.MAPPING_SETTING_MATCH_URL            = "url";
     um.MAPPING_SETTING_MATCH_REGEXP         = "match";
     um.MAPPING_SETTING_REDIRECT_BLOCK       = "block";
