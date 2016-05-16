1129d67cbfdbdae67d60bc67345b34404478c6d1
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-05-16 13:27:17 -0400

    Adds grunt-contrib-connect

---
+++ grunt/connect.js
@@ -0,0 +1,13 @@
+"use strict";
+
+module.exports = {
+
+    test: {
+        options: {
+            port: 9000,
+            keepalive: true,
+            open: "http://localhost:9000/test/host/web.html"
+        }
+    }
+
+};
