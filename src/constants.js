64764dc102073933b514e607f4717a00b3f0ce48
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-05-17 12:43:37 -0400

    ESLint fix

--- src/constants.js
+++ src/constants.js
@@ -1,25 +1,33 @@
 (function () {
     "use strict";
 
-    um.EXTENSION_NAME                       = "chrome-extension-url-mapper";
+    var constants = {
+        EXTENSION_NAME                       : "chrome-extension-url-mapper",
 
-    um.MSG_NO_CONFIGURATION                 = "(click to select a configuration)";
+        MSG_NO_CONFIGURATION                 : "(click to select a configuration)",
 
-    um.CONFIG_NAME                          = "name";
-    um.CONFIG_INJECT                        = "inject";
-    um.CONFIG_MAPPINGS                      = "mappings";
+        CONFIG_NAME                          : "name",
+        CONFIG_INJECT                        : "inject",
+        CONFIG_MAPPINGS                      : "mappings",
 
-    um.MAPPING_SETTING_MATCH_URL            = "url";
-    um.MAPPING_SETTING_MATCH_REGEXP         = "match";
-    um.MAPPING_SETTING_REDIRECT_BLOCK       = "block";
-    um.MAPPING_SETTING_REDIRECT_URL         = "redirect";
-    um.MAPPING_SETTING_OPTION_DEBUG         = "debug";
-    um.MAPPING_SETTING_OPTION_AC_HEADERS    = "ac-headers"; // Access-Control-Allow-Headers
+        MAPPING_SETTING_MATCH_URL            : "url",
+        MAPPING_SETTING_MATCH_REGEXP         : "match",
+        MAPPING_SETTING_REDIRECT_BLOCK       : "block",
+        MAPPING_SETTING_REDIRECT_URL         : "redirect",
+        MAPPING_SETTING_OPTION_DEBUG         : "debug",
+        MAPPING_SETTING_OPTION_AC_HEADERS    : "ac-headers", // Access-Control-Allow-Headers
 
-    um.MSG_INIT_XHR_CONTENT_SCRIPT          = "init-xhr-content-script";
-    um.MSG_QUERY_STATUS                     = "query-status";
-    um.MSG_SET_CONFIGURATION                = "set-configuration";
-    um.MSG_ENABLE_CONFIGURATION             = "enable-configuration";
-    um.MSG_DISABLE_CONFIGURATION            = "disable-configuration";
+        MSG_INIT_XHR_CONTENT_SCRIPT          : "init-xhr-content-script",
+        MSG_QUERY_STATUS                     : "query-status",
+        MSG_SET_CONFIGURATION                : "set-configuration",
+        MSG_ENABLE_CONFIGURATION             : "enable-configuration",
+        MSG_DISABLE_CONFIGURATION            : "disable-configuration"
+    };
+
+    for (var name in constants) {
+        if (constants.hasOwnProperty(name)) {
+            um[name] = constants[name];
+        }
+    }
 
 }());
