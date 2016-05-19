f710be84a0299ec44990ca01e0201ce5b5dfe353
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-05-19 13:34:54 -0400

    Simple test (more coming)

--- test/ajax.js
+++ test/ajax.js
@@ -1,34 +1,60 @@
 describe("Ajax", function () {
     "use strict";
 
-    it("allows asynchronous loading", function (done) {
-        var xhr = new window.XMLHttpRequest();
-        xhr.open("GET", "/test/data/hello.json");
-        xhr.onreadystatechange = function () {
-            if (4 === xhr.readyState) {
-                if (2 === Math.floor(xhr.status / 100)) {
-                    var response = JSON.parse(xhr.responseText);
-                    assert("Hello, World!" === response.message);
-                    done();
-                } else {
-                    done(xhr.responseText);
+    function checkAjaxHandling () {
+
+        it("allows asynchronous loading", function (done) {
+            var xhr = new window.XMLHttpRequest();
+            xhr.open("GET", "/test/data/hello.json");
+            xhr.onreadystatechange = function () {
+                if (4 === xhr.readyState) {
+                    if (2 === Math.floor(xhr.status / 100)) {
+                        var response = JSON.parse(xhr.responseText);
+                        assert("Hello, World!" === response.message);
+                        done();
+                    } else {
+                        done(xhr.responseText);
+                    }
                 }
+            };
+            xhr.send();
+        });
+
+        it("allows synchronous loading", function (done) {
+            var xhr = new window.XMLHttpRequest();
+            xhr.open("GET", "/test/data/hello.json", true);
+            xhr.send();
+            if (2 === Math.floor(xhr.status / 100)) {
+                var response = JSON.parse(xhr.responseText);
+                assert("Hello, World!" === response.message);
+                done();
+            } else {
+                done(xhr.responseText);
             }
-        };
-        xhr.send();
+        });
+
+    }
+
+    describe("Checking unaltered handling", function () {
+        checkAjaxHandling();
     });
 
-    it("allows synchronous loading", function (done) {
-        var xhr = new window.XMLHttpRequest();
-        xhr.open("GET", "/test/data/hello.json", true);
-        xhr.send();
-        if (2 === Math.floor(xhr.status / 100)) {
-            var response = JSON.parse(xhr.responseText);
-            assert("Hello, World!" === response.message);
-            done();
-        } else {
-            done(xhr.responseText);
-        }
+    describe("Installing hook (no interference)", function () {
+
+        var unhook;
+
+        before(function () {
+            um.hook();
+            unhook = window._removeUrlMapperHook;
+            delete window._removeUrlMapperHook;
+        });
+
+        after(function () {
+            unhook();
+        });
+
+        checkAjaxHandling();
+
     });
 
 });
