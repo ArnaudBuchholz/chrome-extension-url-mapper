860afc69bf8c81216296c5fce8dfb4ab6545f3ef
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-06-01 13:22:59 -0400

    Code cleaning

--- src/content/xhr.js
+++ src/content/xhr.js
@@ -8,46 +8,84 @@
      * Communication between the window and the content script is based on synchronous custom events
      */
 
+    /**
+     * Hook event
+     *
+     * @param {String} eventName
+     * @param {Function} handler function handler(eventDetail, answer)
+     */
+    function _hook (eventName, handler) {
+        window.addEventListener("chrome-extension-url-mapper<<xhr::" + eventName, function (eventObject) {
+            handler(eventObject.detail, function (eventDetail) {
+                window.dispatchEvent(new CustomEvent("chrome-extension-url-mapper>>xhr::" + eventName, {
+                    detail: eventDetail
+                }));
+            });
+        }, false);
+    }
+
     var _link = document.createElement("a"); // Will be used to translate relative URL to absolute ones
 
-    window.addEventListener("chrome-extension-url-mapper<<xhr::open", function (event) {
-        var options = new um.MappingOptions(),
-            result,
-            redirectUrl;
-        if (um.configuration) {
-            var args = event.detail;
-            _link.href = args[1];
-            var request = {
-                url: _link.href
-            };
-            result = um.configuration.map(request, options);
-            if (options.debug) {
-                um.log("XMLHttpRequest::open", args, result, options);
+
+    /**
+     * Build the redirection URL
+     *
+     * @param {Object} mapResult Returned by um.configuration.map()
+     * @returns {String|undefined} Possible redirect URL
+     */
+    function _buildRedirectUrl (mapResult) {
+        if (mapResult) {
+            if (mapResult.cancel) {
+                return "about:blank"; // Didn't found better
             }
-            if (result) {
-                if (result.cancel) {
-                    redirectUrl = "about:blank"; // Didn't found better
-                } else if (result.redirectUrl) {
-                    redirectUrl = result.redirectUrl;
-                } else {
-                    return;
-                }
-            }
-            if (redirectUrl) {
-                window.dispatchEvent(new CustomEvent("chrome-extension-url-mapper>>xhr::open", {
-                    detail: {
-                        data: {
-                            method: args[0],
-                            url: redirectUrl
-                        }
-                    }
-                }));
+            if (mapResult.redirectUrl) {
+                return mapResult.redirectUrl;
             }
         }
-    }, false);
+    }
 
-    window.addEventListener("chrome-extension-url-mapper<<xhr::send", function (event) {
-        var data = events.detail.data,
+    /**
+     * Manages the XHR open
+     *
+     * @param {*} eventDetail
+     * @param {Function} answer
+     */
+    function _onXhrOpen (eventDetail, answer) {
+        var options = new um.MappingOptions(),
+            mapResult,
+            redirectUrl;
+        if (um.configuration) {
+            var args = eventDetail,
+                request = {
+                    url: _link.href
+                };
+            _link.href = args[1];
+            mapResult = um.configuration.map(request, options);
+            if (options.debug) {
+                um.log("XMLHttpRequest::open", args, mapResult, options);
+            }
+            redirectUrl = _buildRedirectUrl(mapResult);
+            if (redirectUrl) {
+                answer({
+                    data: {
+                        method: args[0],
+                        url: redirectUrl
+                    }
+                });
+            }
+        }
+    }
+
+    _hook("open", _onXhrOpen);
+
+    /**
+     * Manages the XHR send
+     *
+     * @param {*} eventDetail
+     * @param {Function} answer
+     */
+    function _onXhrSend (eventDetail, answer) {
+        var data = eventDetail.data,
             xhr = new XMLHttpRequest(),
             detail = {};
         xhr.open(data.method, data.url, true /*synchronous*/);
@@ -63,9 +101,9 @@
         ].forEach(function (property) {
             detail[property] = xhr[property];
         });
-        window.dispatchEvent(new CustomEvent("chrome-extension-url-mapper>>xhr::send", {
-            detail: detail
-        }));
-    }, false);
+        answer(detail);
+    }
+
+    _hook("send", _onXhrSend);
 
 }());
